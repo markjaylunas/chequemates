0b5f6f1a9470a37cb3956a23955369afdef79dab
markjaylunas <markjay.lunas@gmail.com>
2025-02-06 17:05:12 +0800

    initial

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import Link from "next/link";
+import * as React from "react";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const links: { title: string; href: string }[] = [
+  {
+    title: "Collections",
+    href: "#",
+  },
+  {
+    title: "Men",
+    href: "#",
+  },
+  {
+    title: "Women",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+  {
+    title: "Contact",
+    href: "#",
+  },
+];
+
+export function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {links.map((link) => (
+          <NavigationMenuItem key={link.title}>
+            <Link href={link.href} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {link.title}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
