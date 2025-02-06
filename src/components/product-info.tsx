b635af9aa5cf76acda9d62719a761d9b4e91395f
markjaylunas <markjay.lunas@gmail.com>
2025-02-06 19:25:27 +0800

    refactor: improvements

--- src/components/product-info.tsx
+++ src/components/product-info.tsx
@@ -4,8 +4,16 @@ import { Badge } from "./ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export default function ProductInfo({ product }: { product: ProductData }) {
-  const { name, brand, description, price, oldPrice, currency, discount } =
-    product;
+  const {
+    name,
+    brand,
+    description,
+    price,
+    oldPrice,
+    currency,
+    discount,
+    stock,
+  } = product;
 
   return (
     <Card className="max-w-md w-full shadow-none text-foreground border-none">
@@ -13,24 +21,37 @@ export default function ProductInfo({ product }: { product: ProductData }) {
         <h4 className="text-muted-foreground text-sm uppercase tracking-wide font-semibold">
           {brand}
         </h4>
-        <CardTitle className="text-4xl font-bold">{name}</CardTitle>
+        <CardTitle className="text-4xl font-bold">
+          <h1>{name}</h1>
+        </CardTitle>
       </CardHeader>
       <CardContent className="space-y-4">
         <p className="text-muted-foreground mb-4">{description}</p>
         <div className="space-y-2">
           <div className="flex items-center space-x-2">
-            <span className="text-black text-2xl font-bold">
-              {currency}
-              {price.toFixed(2)}
+            <span
+              className="text-black text-2xl font-bold"
+              aria-label={`Current Price: ${currency} ${price.toFixed(2)}`}
+            >
+              {currency} {price.toFixed(2)}
             </span>
-            <Badge className="bg-black text-white p-1 px-2">{discount}%</Badge>
+            {discount > 0 && (
+              <Badge className="bg-black text-white p-1 px-2">
+                {discount}%
+              </Badge>
+            )}
           </div>
-          <span className="text-sm font-semibold line-through text-muted-foreground">
-            {currency}
-            {oldPrice.toFixed(2)}
-          </span>
+          {oldPrice ? (
+            <span className="text-sm font-semibold line-through text-muted-foreground">
+              {currency} {oldPrice.toFixed(2)}
+            </span>
+          ) : null}
         </div>
-        <AddToCartForm productId={product.id} stock={product.stock} />
+        {stock === 0 ? (
+          <span className="text-red-500">Out of Stock</span>
+        ) : (
+          <AddToCartForm productId={product.id} stock={stock} />
+        )}
       </CardContent>
     </Card>
   );
